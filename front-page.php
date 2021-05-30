4065e66570762841e100ec99b82cf7ca7e58ae66
den <justjudobest@mail.ru>
2021-05-30 15:10:40 +0200

    block

--- front-page.php
+++ front-page.php
@@ -1,123 +1,127 @@
-<?php 
+<?php
 get_header();
 
 $slider = get_field('main-slider', 14);
-$advantages_block = get_field('advantages_block', 14);
-$ordering_procedure = get_field('ordering_procedure', 14);
-$promo_block= get_field('promo_block', 14);
+$advantages = get_field('advantages_block', 14);
+$ordering= get_field('ordering_procedure', 14);
+$promo = get_field('promo_block', 14);
 ?>
-<main>
-    <section id="front-slider">
-        <div class="swiper-container front-slider">
-            <div class="swiper-wrapper">
-                <?php foreach ($slider as $slide) {
-                $examples = $slide['examples'];
-                ?>
-                <div class="swiper-slide">
-                    <div class="slider-content">
-                        <div class="about">
-                            <p><?= $slide['description'] ?></p>
-                            <h2><?= $slide['title'] ?></h2>
-                            <?php if ($slide['link']) { ?>
-                            <a href="<?= $slide['link'] ?>">Подробнее</a>
-                            <?php } ?>
-                        </div>
+    <main>
+        <section id="front-slider">
+            <div class="swiper-container front-slider">
+                <div class="swiper-wrapper">
+                  <?php foreach ($slider as $slide) {
+                    $examples = $slide['examples'];
+                    ?>
+                      <div class="swiper-slide">
+                          <div class="slider-content">
+                              <div class="about">
+                                  <p><?= $slide['description'] ?></p>
+                                  <h2><?= $slide['title'] ?></h2>
+                                <?php if ($slide['link']) { ?>
+                                    <a href="<?= $slide['link'] ?>">Подробнее</a>
+                                <?php } ?>
+                              </div>
 
-                        <div class="examples">
-                            <?php foreach($examples as $examlpe): 
-                              ?>
-                            <span>
+                              <div class="examples">
+                                <?php foreach($examples as $examlpe):
+                                  ?>
+                                    <span>
                                 <img src="<?=$examlpe;?>" alt="example">
                             </span>
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                    <img class="slide-main-image" src="<?php echo $slide['image']['url']; ?>" alt="slide">
-                </div>
-                <?php } ?>
-            </div>
-            <div class="swiper-pagination"></div>
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
-        </div>
-    </section>
-    <section id="advantages">
-        <div class="adv_layout_image">
-            <?php if ($advantages['background_image']) : ?>
-            <img src="<?= $advantages['background_image']['url'] ?>" alt="">
-            <?php endif; ?>
-        </div>
-
-        <div class="advantages_texts">
-            <p><?= $advantages['subheadline'] ?? ''?></p>
-            <h2><?= $advantages['headline'] ?? ''?></h2>
-        </div>
-
-        <?php if ($advantages['Advantage']) : ?>
-        <div class="adv_items">
-
-            <?php foreach ($advantages['Advantage'] as $advantage) : ?>
-            <div class="avd_item">
-                <?php if ($advantage['image']) : ?>
-                <img src="<?= $advantage['image']['url']?>" alt="">
-                <?php endif; ?>
-
-                <p class="adv_info">
-                    <?= $advantage['description_image'] ?? ''?>
-                </p>
-            </div>
-            <?php endforeach; ?>
-
-        </div>
-        <?php endif; ?>
-
-
-    </section>
-    <section id="ordering" data-parallax="scroll" positionY="0px"
-        data-image-src="<?= $ordering['background_image']['url'] ?>">
-
-        <?php if ($ordering['description_of_ordering_procedure']) : ?>
-        <div class="ordering-procedure">
-            <?php foreach ($ordering['description_of_ordering_procedure'] as $order) : ?>
-            <div class="order-item">
-                <p><?= $order['headline'] ?></p>
-                <?php if ($order['image']) : ?>
-                <img src="<?= $order['image']['url'] ?>" alt="">
-                <?php endif; ?>
-                <p><?= $order['description'] ?></p>
-            </div>
-
-            <?php endforeach; ?>
-        </div>
-        <?php endif; ?>
-    </section>
-    <section class="promo-block">
-        <div class="container">
-            <div class="
-            <?= $promo_block['display_position'] ? 'promo-block-wrapper__' . $promo_block['display_position'] : '' ?>">
-                <div class="promo-block-left">
-                    <div class="promo-block-left-content">
-                      <?= $promo_block['headline'] ?? '' ?>
-                    </div>
-                  <?php if ($promo_block['images']) : ?>
-                    <?php foreach ($promo_block['images'] as $image) : ?>
-                          <div class="promo-block-left-content">
-                              <img src="<?= $image['image']['url'] ?>" alt="">
+                                <?php endforeach; ?>
+                              </div>
                           </div>
-                    <?php endforeach; ?>
-                  <?php endif; ?>
+                          <img class="slide-main-image" src="<?php echo $slide['image']['url']; ?>" alt="slide">
+                      </div>
+                  <?php } ?>
                 </div>
-                <div class="promo-block-right">
-                    <div class="promo-block-description">
-                      <?= $promo_block['description'] ?? '' ?>
-                    </div>
-                </div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
             </div>
-        </div>
-    </section>
-    <section></section>
-    <section></section>
-    <section></section>
-</main>
+        </section>
+        <section id="advantages">
+            <div class="adv_layout_image">
+              <?php if ($advantages['background_image']) : ?>
+                  <img src="<?= $advantages['background_image']['url'] ?>" alt="">
+              <?php endif; ?>
+            </div>
+
+            <div class="advantages_texts">
+                <p><?= $advantages['subheadline'] ?? ''?></p>
+                <h2><?= $advantages['headline'] ?? ''?></h2>
+            </div>
+
+          <?php if ($advantages['Advantage']) : ?>
+              <div class="adv_items">
+
+                <?php foreach ($advantages['Advantage'] as $advantage) : ?>
+                    <div class="avd_item">
+                      <?php if ($advantage['image']) : ?>
+                          <img src="<?= $advantage['image']['url']?>" alt="">
+                      <?php endif; ?>
+
+                        <p class="adv_info">
+                          <?= $advantage['description_image'] ?? ''?>
+                        </p>
+                    </div>
+                <?php endforeach; ?>
+
+              </div>
+          <?php endif; ?>
+
+
+        </section>
+        <section id="ordering" data-parallax="scroll" positionY="0px"
+                 data-image-src="<?= $ordering['background_image']['url'] ?>">
+
+          <?php if ($ordering['description_of_ordering_procedure']) : ?>
+              <div class="ordering-procedure">
+                <?php foreach ($ordering['description_of_ordering_procedure'] as $order) : ?>
+                    <div class="order-item">
+                        <p><?= $order['headline'] ?></p>
+                      <?php if ($order['image']) : ?>
+                          <img src="<?= $order['image']['url'] ?>" alt="">
+                      <?php endif; ?>
+                        <p><?= $order['description'] ?></p>
+                    </div>
+
+                <?php endforeach; ?>
+              </div>
+          <?php endif; ?>
+        </section>
+        <section class="promo-block">
+            <div class="container">
+              <?php if ($promo['block']) : ?>
+                <?php foreach ($promo['block'] as $block) : ?>
+                      <div class="
+            <?= $block['display_position'] ? 'promo-block-wrapper__' . $block['display_position'] : '' ?>">
+                          <div class="promo-block-left">
+                              <div class="promo-block-left-content">
+                                <?= $block['headline'] ?? '' ?>
+                              </div>
+                            <?php if ($block['images']) : ?>
+                              <?php foreach ($block['images'] as $image) : ?>
+                                    <div class="promo-block-left-content">
+                                        <img src="<?= $image['image']['url'] ?>" alt="">
+                                    </div>
+                              <?php endforeach; ?>
+                            <?php endif; ?>
+                          </div>
+                          <div class="promo-block-right">
+                              <div class="promo-block-description">
+                                <?= $block['description'] ?? '' ?>
+                              </div>
+                          </div>
+                      </div>
+                <?php endforeach;?>
+              <?php endif;?>
+            </div>
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+    </main>
 
 <?php get_footer(); ?>
