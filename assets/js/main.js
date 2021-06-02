7f67c36eeb68431f77e9cb1d6bb26277864e12ec
Ruslan Matusevich <matusevich@teamidea.ru>
2021-06-02 20:37:54 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,11 +13,12 @@ $(document).ready(function () {
     slidesPerView: 1,
     speed: 600,
     centeredSlides: true,
+    loop: true,
     // autoplay: {
     //   delay: 4000,
     //   disableOnInteraction: false,
     // },
-    autoplay: false,
+    // autoplay: false,
     spaceBetween: 0,
     navigation: {
       nextEl: ".swiper-button-next",
@@ -38,9 +39,7 @@ $(document).ready(function () {
     //   delay: 4000,
     //   disableOnInteraction: false,
     // },
-    autoplay: false,
-    allowTouchMove: false,
-    spaceBetween: 50,
+    spaceBetween: 10,
     navigation: {
       nextEl: ".swiper-button-next-сertificate",
       prevEl: ".swiper-button-prev-сertificate",
@@ -60,5 +59,134 @@ $(document).ready(function () {
     //     slidesPerView: 'auto',
     //   }
     // }
+  });
+
+
+  //Form Checker
+  function checkFormControl() {
+    $(".form-control").each(function () {
+      var thisa = $(this);
+      if (thisa.val().length) {
+        thisa.parents(".form-field").addClass("focus");
+        thisa.parents(".form-field").addClass("validate");
+      }
+    });
+  }
+  checkFormControl();
+  var $formField = $(".form-field");
+  $formField.on("focus", ".form-control", function () {
+    $(this).parents(".form-field").addClass("focus");
+    $(this).parents(".form-field .form-control").addClass("validate");
+  });
+
+  $formField.on("focusout", ".form-control", function () {
+    if (!$(this).val().length) {
+      $(this).parents(".form-field").removeClass("focus");
+    } else {}
+  });
+
+
+  //Mask 
+  $('input[type="tel"]').mask('+7(000)000-00-00');
+  //Masonry
+
+  const masonry = $('.works-gallery').masonry({
+    // options
+    itemSelector: '.gallery-item',
+    singleMode: false,
+    isResizable: true,
+    isAnimated: true,
+    animationOptions: {
+      queue: false,
+      duration: 500
+    }
+  });
+  setTimeout(() => {
+    masonry.masonry()
+
+  }, 100)
+
+  //Quiz
+  $('.step-one .variable').click(function () {
+    $('.step-one').fadeOut(500);
+
+    const value = $(this).attr('data-value');
+    $('input[name="system"]').val(value);
+
+    setTimeout(() => {
+      $('.step-two').fadeIn(500)
+      $('.step-one').removeClass('active');
+      $('.step-two').addClass('active')
+      $('.step-one').removeClass('active')
+
+    }, 500)
+  })
+
+  $('.step-two .variable').click(function () {
+
+    const value = $(this).attr('data-value');
+    $('input[name="project"]').val(value);
+
+    $('.step-two').fadeOut(500);
+    setTimeout(() => {
+      $('.step-three').fadeIn(500)
+      $('.step-two').removeClass('active');
+      $('.step-three').addClass('active')
+      $('.step-two').removeClass('active')
+    }, 500)
+  })
+
+  $('.step-three .variable').click(function () {
+
+    const value = $(this).attr('data-value');
+    $('input[name="youAre"]').val(value);
+
+    $('.step-three').fadeOut(500);
+    setTimeout(() => {
+      $('.step-four').fadeIn(500)
+      $('.step-three').removeClass('active');
+      $('.step-four').addClass('active')
+      $('.step-three').removeClass('active')
+    }, 500)
+  })
+
+  $('.step-four .variable').click(function () {
+
+    const value = $(this).attr('data-value');
+    $('input[name="contact"]').val(value);
+
+    $('.step-four').fadeOut(500);
+    setTimeout(() => {
+      $('.step-five').fadeIn(500)
+      $('.step-four').removeClass('active');
+      $('.step-five').addClass('active')
+      $('.step-four').removeClass('active')
+    }, 500)
+  })
+
+  $('.last-step-form input[name="f-Name"]').change(function (e) {
+    const value = $(this).val();
+    $('input[name="userName"]').val(value)
+  })
+
+  $('.last-step-form input[name="f-Phone"]').change(function (e) {
+    const value = $(this).val();
+    $('input[name="userPhone"]').val(value)
+  })
+
+
+  $('.to-top').click(function () {
+    window.scrollTo(0, 0);
+  })
+
+
+  $(window).scroll(function () {
+    scroll = $(window).scrollTop();
+
+    if (scroll >= 500) {
+      $('.to-top').fadeIn(500)
+    } else {
+      $('.to-top').fadeOut(500)
+    }
   });
 });
--- functions.php
+++ functions.php
@@ -3,15 +3,20 @@ function luminotti_scripts_styles()
 {
   wp_register_style('swiper_css', get_template_directory_uri() . '/assets/css/swiper.css', array(), '1.0', 'screen');
   wp_register_style('animate', get_template_directory_uri() . '/assets/css/animate.css', array(), '1.0', 'screen');
+  wp_register_style('fancybox_style', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(), '1.2', 'screen');
   wp_register_style('my_style', get_template_directory_uri() . '/assets/css/main.css', array(), '1.2', 'screen');
   wp_register_style('promo-block', get_template_directory_uri() . '/assets/css/promo-block.css', array(), '1.2', 'screen');
 
   wp_enqueue_style('swiper_css');
   wp_enqueue_style('animate');
+  wp_enqueue_style('fancybox_style');
   wp_enqueue_style('my_style');
   wp_enqueue_style('promo-block');
 
   wp_enqueue_script('jquery_js', get_template_directory_uri() . '/assets/js/jquery.js', array(), '1.0', true);
+  wp_enqueue_script('fancybox_js', get_template_directory_uri() . '/assets/js/jquery.fancybox.min.js', array(), '1.0', true);
+  wp_enqueue_script('mask', get_template_directory_uri() . '/assets/js/jquery.mask.js', array(), '1.0', true);
+  wp_enqueue_script('masonry', get_template_directory_uri() . '/assets/js/masonry.js', array(), '1.0', true);
   wp_enqueue_script('plugins', get_template_directory_uri() . '/assets/js/plugins.js', array(), '1.0', true);
   wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0', true);
   wp_localize_script('main', 'ajax', [
